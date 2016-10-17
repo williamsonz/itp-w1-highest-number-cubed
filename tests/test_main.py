d76b8a6b4a05f8f568dbc037a88528d546f202aa
Santiago Basulto <santiago.basulto@gmail.com>
2016-10-17 18:51:39 -0300

    Initial commit

---
+++ tests/test_main.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+import unittest
+
+from highest_number_cubed import main
+
+
+class TestHighestNumberCubed(unittest.TestCase):
+
+    def test_simple(self):
+        self.assertEqual(1 + 1, 2)
